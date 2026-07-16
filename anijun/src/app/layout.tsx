5dd7371446124a89c7d9d1eeaf529511b24b3582
konstasil <konstasil777@gmail.com>
2026-07-16 13:14:24 +0000

    modified:   src/app/layout.tsx src/components/ErudaConsole.tsx

--- anijun/src/app/layout.tsx
+++ anijun/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
+import ErudaConsole from "@/components/ErudaConsole";
 
 const inter = Inter({
   variable: "--font-geist-sans",
@@ -21,6 +22,7 @@ export default function RootLayout({
   return (
     <html lang="ru" className={`${inter.variable} h-full antialiased`}>
       <body className="min-h-full flex flex-col custom-scrollbar">
+        <ErudaConsole />
         <Header />
         <main className="flex-1 max-w-[1600px] w-full mx-auto p-6">
           {children}
